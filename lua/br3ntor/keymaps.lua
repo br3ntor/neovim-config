19c95b4084bad4d21f65740bf9ac526928386ecf
br3ntor <br3ntor@gmail.com>
2023-07-10 22:02:40 -0700

    f3 to toggle line nums

--- lua/br3ntor/keymaps.lua
+++ lua/br3ntor/keymaps.lua
@@ -7,10 +7,14 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Buffer controls
 vim.keymap.set('n', '<leader>p', ':bprevious<CR>', {})
 vim.keymap.set('n', '<leader>n', ':bnext<CR>', {})
 vim.keymap.set('n', '<leader>q', ':bdelete<CR>', {})
 
--- My custom not suggested by anyone
 -- Ctrl-S save
 vim.keymap.set('n', '<C-s>', ':w<CR>', {})
+
+-- Toggle line numbers
+vim.keymap.set('n', '<F3>', ':set invnumber invrelativenumber<CR>', {})
